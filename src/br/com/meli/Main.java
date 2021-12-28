68d13ec69863e2f748c79481b9b072ce737e5a65
joseismaelrj <jose.nascimento@mercadolivre.com>
2021-12-27 21:20:58 -0300

    UpLoad do projeto Java1-TT1

---
+++ src/br/com/meli/Main.java
@@ -0,0 +1,10 @@
+package br.com.meli;
+
+public class Main {
+    public static void main(String[] args) {
+        int iArr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8 };
+
+        RadixSort rs = new RadixSort();
+        rs.radixSort(iArr);
+    }
+}

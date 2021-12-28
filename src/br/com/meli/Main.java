a2139f2c39602f6098125b8360f0cfc65f62b8b5
joseismaelrj <jose.nascimento@mercadolivre.com>
2021-12-27 21:58:16 -0300

    Correção de erro do projeto

--- src/br/com/meli/Main.java
+++ src/br/com/meli/Main.java
@@ -1,8 +1,8 @@
 package br.com.meli;
 
 public class Main {
-    public static void main(String[] args) {
-        int iArr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8 };
+    public static void main(String[] arqs){
+        int[] iArr = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8 };
 
         RadixSort rs = new RadixSort();
         rs.radixSort(iArr);
